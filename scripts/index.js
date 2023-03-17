85697eae373b48c7c6e621d3b089dfc55d33ec1a
Elizaveta Parada <parada_ev@mail.ru>
2023-03-17 21:20:50 +0100

    fin

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,12 @@
 const popupElement = document.querySelector(".popup");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close");
 const popupOpenButtonElement = document.querySelector(".profile__editbutton");
+const nameInput = popupElement.querySelector(".popup__subtitle-name");
+const jobInput = popupElement.querySelector(".popup__subtitle-job");
+const addButtonElement = popupElement.querySelector(".form__submit-btn_action_add");
+const profileTitle = document.querySelector(".profile__info-title");
+const profileSubtitle = document.querySelector(".profile__info-subtitle");
+  
 
 const openPopup = function() {
     popupElement.classList.add("popup_is-opened");
@@ -14,25 +20,19 @@ const closePopup = function() {
 popupOpenButtonElement.addEventListener("click", openPopup);
 popupCloseButtonElement.addEventListener("click", closePopup);
 
-// Находим форму в DOM
-// const formElement = document.querySelector(".popup");
-// Находим поля формы в DOM
-// const nameInput = formElement.querySelector(".popup__subtitle-name");
-// const jobInput = formElement.querySelector(".popup__subtitle-job");
-// const addButtonElement = formElement.querySelector(".form__submit-btn_action_add");
-
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-// function handleFormSubmit (evt) {
-    // evt.preventDefault(); 
-    // Получите значение полей jobInput и nameInput из свойства value
-
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
     // Выберите элементы, куда должны быть вставлены значения полей
-
     // Вставьте новые значения с помощью textContent
-// }
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+    closePopup()
+    
+}
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-// formElement.addEventListener('submit', handleFormSubmit); 
+popupElement.addEventListener('submit', handleFormSubmit); 
